9e6a92ece58eb2bd2a52c1adefecccc20f47af3b
UZB02 <muhsinbekmirzamatov3@gmail.com>
2024-03-29 20:59:08 +0500

    Proekt qo'shish qismi

--- src/views/Projects/ProjectsList/AddProject.vue
+++ src/views/Projects/ProjectsList/AddProject.vue
@@ -1,81 +1,167 @@
 <template>
     <section>
-       <div class="card">
-        <form>
-            <div class="grid gap-3 md:grid-cols-4">
-                <div>
-                    <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Loyiha nomi</label>
-                    <input v-model="name" type="text" id="first_name"
-                        class="border border-gray-300  text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="Project Menagment" />
+        <div class="card">
+            <form @submit.prevent="addproject" class="w-full flex flex-col items-center justify-center gap-3">
+                <div class="grid gap-3 md:grid-cols-4">
+                    <div>
+                        <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Loyiha nomi</label>
+                        <input
+                            v-model="name"
+                            type="text"
+                            id="first_name"
+                            class="border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="Project Menagment"
+                        />
+                    </div>
+                    <div>
+                        <label for="last_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Loyiha nomi qisqartmasi</label>
+                        <input
+                            v-model="shortname"
+                            type="text"
+                            id="last_name"
+                            class="border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="PM"
+                        />
+                    </div>
+                    <div>
+                        <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300" for="file_input">Logo</label>
+                        <input
+                            class="border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            id="file_input"
+                            @change="handleFileChange"
+                            type="file"
+                        />
+                    </div>
+                    <div>
+                        <label for="startT" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">loyihani rejalashtirilgan start sanasi</label>
+                        <input
+                            type="datetime-local"
+                            v-model="date_create"
+                            id="startT"
+                            class="border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="123-45-678"
+                            pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
+                        />
+                    </div>
+                    <div>
+                        <label for="EndT" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">loyihaning rejalashtirilgan tugash sanasi</label>
+                        <input
+                            type="datetime-local"
+                            v-model="end_date"
+                            id="EndT"
+                            class="border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="123-45-678"
+                            pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
+                        />
+                    </div>
+                    <div>
+                        <label for="summ" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">loyihanin ralizatsiyasiga ajratilgan summa</label>
+                        <input
+                            type="number"
+                            v-model="budget"
+                            id="summ"
+                            class="border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="500 000"
+                            min="0"
+                        />
+                    </div>
+                    <div class="flex flex-col">
+                        <label for="visitors" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">loyiha belgilangan rangi </label>
+                        <span class="flex items-center justify-center"> <ColorPicker v-model="color" /></span>
+                    </div>
+                    <div>
+                        <select
+                            name=""
+                            id=""
+                            class="border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 mt-4 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        >
+                            <option value="manolith">Manolith</option>
+                            <option value="subsystem">Sub-sytem</option>
+                        </select>
+                    </div>
                 </div>
-                <div>
-                    <label for="last_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Loyiha nomi qisqartmasi</label>
-                    <input type="text" id="last_name"
-                        class="border border-gray-300  text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="PM" />
-                </div>
-                <div>
-                    <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                        for="file_input">Logo</label>
-                    <input
-                        class="border border-gray-300  text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        id="file_input" type="file">
-                </div>
-                <div>
-                    <label for="phone" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">loyihani rejalashtirilgan start sanasi</label>
-                    <input type="datetime-local" id="phone"
-                        class="border border-gray-300  text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="123-45-678" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" />
-                </div>
-                <div>
-                    <label for="phone" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">loyihaning rejalashtirilgan tugash sanasi</label>
-                    <input type="datetime-local" id="phone"
-                        class="border border-gray-300  text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="123-45-678" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" />
-                </div>
-                <div>
-                    <label for="summ" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">loyihanin ralizatsiyasiga ajratilgan summa</label>
-                    <input type="number" id="summ"
-                        class="border border-gray-300  text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="500 000" min="0" />
-                </div>
-                <div class="flex flex-col">
-                    <label for="visitors" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">loyiha belgilangan rangi </label>
-       <span class="flex items-center justify-center"> <ColorPicker v-model="color" /></span>
-                </div>
-               <div>
-                <select name="" id="" class="border border-gray-300  text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 mt-4 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                    <option value="manolith">Manolith</option>
-                    <option value="subsystem">Sub-sytem</option>
-                </select>
-               </div>
-            </div>
-        <button @click="addProject" type="submit"
-            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add</button>
-    </form>
-       </div>
-    <!-- <ul v-for="item in data" :key="item.id">
-        <li>{{ item }} <button class="btn btn-danger bg-red-600" @click="deleteList(item)">Delete</button></li>
-    </ul> -->
-</section></template>
+                <span class="w-full flex gap-2">
+                    <button
+                        @click="addproject"
+                        type="button"
+                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    >
+                        Add
+                    </button>
+                    <button
+                        type="submit"
+                        class="text-white bg-red-500 hover:bg-red-400 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
+                    >
+                        Cencel
+                    </button>
+                </span>
+            </form>
+        </div>
+    </section>
+</template>
 <script setup>
-import { ref, reactive,toRaw } from 'vue';
-let name = ('')
-let data = ([])
+import { ref } from 'vue';
+import axios from 'axios';
+import Swal from 'sweetalert2';
+const name = ref('');
+const shortname = ref('');
+const date_create = ref('');
+const end_date = ref('');
+const budget = ref('');
+const color = ref('');
+const logo = ref('');
 
-const addProject = () => {
-    data.push(name)
-    name =''
-}
-// const deleteList = (item) => {
-//     console.log(6565);
-//     const index = data.indexOf(item)
-//     if (index > -1) {
-//         data.splice(index, 1);
-//     }
-// }
+// let data=({
+//     name: '',
+//     shortname: '',
+//     date_create: '',
+//     end_date: '',
+//     budget: '',
+//     logo: '',
+//     color: '',
+// })
 
+const addproject = () => {
+    axios
+        .post(
+            'https://pm-api.essential.uz/api/project/create?name%20=Project1',
+            {
+                name: name.value,
+                shortname: shortname.value,
+                date_create: date_create.value,
+                end_date: end_date.value,
+                budget: budget.value,
+                logo: logo.value,
+                color: color.value
+            },
+            {
+                headers: {
+                    Authorization: 'Bearer ' + localStorage.getItem('token')
+                }
+            }
+        )
+        .then((res) => {
+            if (res.status === 200) {
+                Swal.fire({
+                    position: 'top-center',
+                    icon: 'success',
+                    title: 'Bajarildi',
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+                name.value = '';
+                shortname.value = '';
+                date_create.value = '';
+                end_date.value = '';
+                budget.value = '';
+                logo.value = '';
+                color.value = '';
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+};
 </script>
 
 <style scoped></style>
