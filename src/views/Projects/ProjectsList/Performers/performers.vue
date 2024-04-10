4ff6fa3e9bc7a3217ebe2b466ebf962e3881402f
UZB02 <muhsinbekmirzamatov3@gmail.com>
2024-04-10 21:00:59 +0500

    Loyiha ijrochilarini qo'shish bajarildi

--- src/views/Projects/ProjectsList/Performers/performers.vue
+++ src/views/Projects/ProjectsList/Performers/performers.vue
@@ -56,20 +56,20 @@
                         </span>
                     </span>
                 </div>
-                <div class="bottom flex items-center gap-3 flex-wrap">
+                <div class="bottom flex items-center justify-between gap-3 flex-wrap">
                     <div v-for="item in comunitiCard"
                         class="card1 transition hover:scale-[1.02] flex items-center justify-center flex-col gap-2 shadow rounded-xl p-3 w-[19%] max-[900px]:w-[30%] max-[1030px]:w-[45%]">
                         <div class="action w-full flex items-center justify-end gap-2">
-                            <i @click="toggle" aria-haspopup="true" aria-controls="overlay_menu1"
+                            <i @click="getPerformers(item.id)" aria-haspopup="true" aria-controls="overlay_menu1"
                                 class="pi pi-ellipsis-h cursor-pointer"></i>
                         </div>
                         <span class="flex items-center justify-center flex-col gap-2  p-3 w-full">
-                            <!-- <img @click="gotoPerformersInfo(item.id)" :src="item.img"
-                                class="w-24 card-img  h-24 rounded-[50%] cursor-pointer" alt=""> -->
-                            <!-- <h1 class="font-bold">{{ item.name }}</h1>
-                            <h2 class="font-sans font-medium">{{ item.level }}</h2>
+                            <img @click="gotoPerformersInfo(item.id)" src="https://avatars.mds.yandex.net/i?id=738b728f5728fc4d9b1bb45e0c787450ab62c59b-10705627-images-thumbs&n=13"
+                                class="w-24 card-img  h-24 rounded-[50%] cursor-pointer" alt="">
+                            <h1 class="font-bold">{{ item.user.fio}}</h1>
+                            <h2 class="font-sans font-medium">{{ item.status }}</h2>
                             <h4>{{ item.rols }}</h4>
-                            <h5 class="text-gray-500 font-italic">{{ item.tell }}</h5> -->
+                            <h5 class="text-gray-500 font-italic">{{ item.user.phone }}</h5>
                         </span>
                     </div>
                     <Menu ref="menu" :model="overlayMenuItems" :popup="true" />
@@ -78,7 +78,7 @@
             <div :class="card_table ? 'hidden' : 'list w-full max-[900px]:w-[100%]'">
                 <div class="card">
                     <div class="flex align-items-center justify-content-between mb-4">
-                        <h5 class="text-4xl font-medium">6 Performers</h5>
+                        <h5 class="text-4xl font-medium">{{ comunitiCard.length }} Performers</h5>
                         <div class="left flex items-center justify-center gap-3">
                             <span class="flex items-center justify-center gap-3">
                                 <i class="pi pi-sort-amount-up text-2xl cursor-pointer transition hover:text-gray-400"></i>
@@ -92,46 +92,47 @@
                         </div>
                         </div>
                     </div>
-                    <ul v-for="item in list" :key="item.id" class="w-full p-0 mx-0 mt-0 mb-4 list-none">
+                    <ul v-for="(item,itemkey) in comunitiCard" :key="item.id" class="w-full p-0 mx-0 mt-0 mb-4 list-none">
                         <li
                             class="flex items-center justify-between  py-2 border-bottom-1 max-[900px]:w-[90%] surface-border">
                             <div class="w-[35%] flex items-center gap-2">
-                                <h1 class="font-bold text-gray-500">{{ item.id }}.</h1>
+                                <h1 class="font-bold text-gray-500">{{ itemkey + 1 }}.</h1>
                                 <span class="flex w-1/4 items-center justify-center gap-2">
-                                    <Avatar @click="router.push(`/performersinfo`)" class="cursor-pointer" :image="item.avatar" size="large" shape="circle">
+                                    <Avatar @click="router.push(`/performersinfo`)" class="cursor-pointer" image="https://avatars.mds.yandex.net/i?id=738b728f5728fc4d9b1bb45e0c787450ab62c59b-10705627-images-thumbs&n=13" size="large" shape="circle">
                                     </Avatar>
                                 </span>
 
                                 <span @click="generalinformation"
                                     class="w-[70%] cursor-pointer text-900 line-height-3 flex flex-col gap-2">
                                     <h1 class="font-bold whitespace-nowrap overflow-hidden text-overflow-ellipsis">{{
-                                        item.avatar_name }}</h1>
-                                    <h4 class="text-slate-400">{{ item.level }}</h4>
+                                        item.user.fio }}</h1>
+                                    <h4 class="text-slate-400">{{ item.user.username }}</h4>
                                 </span>
                             </div>
                             <div class="w-[65%] flex  gap-3 items-center justify-between">
                                 <span class="flex items-center justify-center gap-1">
-                                    <Tag class="w-[65px]" :severity="item.severity" :value="item.icon_value"></Tag>
+                                    <h1>{{ item.user.status }}</h1>
+                                    <!-- <Tag class="w-[65px]" :severity="item.severity" :value="item.icon_value"></Tag> -->
                                 </span>
                                 <span class="flex items-center justify-center gap-2">
                                     <i class="pi pi-calendar"></i>
                                     <span class="w-40 font-semibold">
-                                        {{ item.month }} - {{ item.term }}
+                                        {{ item.user.created_date }}
                                     </span>
                                 </span>
 
                                 <span class="flex items-center justify-center gap-2">
                                     <i class="pi pi-paperclip"></i>
-                                    <h3>{{ item.files }}</h3>
+                                    <h3>5</h3>
                                 </span>
                                 <span class="w-1/4 flex flex-col items-center  justify-center gap-1">
                                     <div class=" w-full flex  items-center  justify-center gap-3">
                                         <span class="bg-gray-200 flex items-center rounded-xl w-full">
-                                            <div :style="{ width: `${item.score}` }"
+                                            <div :style="{ width: '50%' }"
                                                 class="score rounded-xl bg-green-500 h-2">
                                             </div>
                                         </span>
-                                        <span class="text-sm">{{ item.score }}</span>
+                                        <span class="text-sm">50%</span>
                                     </div>
                                 </span>
                                 <div class="actions flex items-center justify-center gap-3">
@@ -269,6 +270,23 @@
                 <levelPerformers />
             </div> -->
         </div>
+           <!-- Begin Modal Delet -->
+        <Dialog v-model:visible="deletModal" header="Delet Project" :style="{ width: '25rem' }">
+            <div class="p-2 pt-0 text-center">
+                <svg class="w-20 h-20 text-red-600 mx-auto" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                </svg>
+                <h3 class="text-xl font-normal text-gray-500 mt-5 mb-6">O'chirishni istaysizmi?</h3>
+                <button @click="deletePerformers()" class="text-white bg-red-600 hover:bg-red-300 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-base inline-flex items-center px-3 py-2.5 text-center mr-2">
+                    <ProgressSpinner style="width: 20px; height: 20px" :class="loadingDel ? 'block' : 'hidden'" strokeWidth="8" fill="var(--surface-ground)" animationDuration=".5s" aria-label="Custom ProgressSpinner" />
+                    <span :class="loadingDel ? 'block' : 'hidden'">Loading...</span> <span :class="loadingDel ? 'hidden' : 'block'">O'chirish</span>
+                </button>
+                <button @click="deletModal = false" class="text-gray-900 bg-white hover:bg-gray-100 focus:ring-4 focus:ring-cyan-200 border border-gray-200 font-medium inline-flex items-center rounded-lg text-base px-3 py-2.5 text-center">
+                    Bekor qilish
+                </button>
+            </div>
+        </Dialog>
+        <!-- End Modal Delet -->
 
         <Dialog v-model:visible="visible" maximizable modal header="Header" :style="{ width: '70rem' }"
             :breakpoints="{ '1199px': '75vw', '575px': '90vw' }">
@@ -375,11 +393,51 @@ const doneTask = 40;
 const value = ref(Math.round((doneTask / allTask) * 100));
 const project_id=ref(router.currentRoute.value.params.id)
 console.log(project_id.value);
+const performers_id=ref('')
+const deletModal = ref(false);
+const loadingDel = ref(false);
+
 
 const gotoPerformersInfo=(id)=>{
     router.push('/projects_list/:id/performers/:performers_id/performersinfo')
     console.log(id);
 }
+const modalDelet = () => {
+    deletModal.value = true;
+};
+
+function getPerformers (id){
+    performers_id.value = id;
+    console.log(id);
+     toggle(event);
+}
+
+const deletePerformers=()=>{
+     loadingDel.value = true;
+    axios.delete(`https://pm-api.essential.uz/api/performers/${performers_id.value}/delete`, {
+        headers: {
+            Authorization: 'Bearer ' + localStorage.getItem('token')
+        }
+    }).then((res) => {
+        if (res.status === 200) {
+            Swal.fire({
+                    position: 'top-center',
+                    icon: 'success',
+                    title: `O'chirildi`,
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+            loadingDel.value = false;
+            deletModal.value = false;
+            fetchPerformers();
+        }
+        console.log(res);
+    }).catch((err) => {
+        console.log(err);
+    })
+
+}
+
 const comunitiCard = ref({})
 
 function fetchPerformers() {
@@ -448,13 +506,16 @@ const overlayMenuItems = [
     },
     {
         label: 'Taxrirlash',
-        icon: 'pi pi-pencil'
+        icon: 'pi pi-pencil',
+        command: () => {
+            console.log(652);
+        }
     },
     {
         label: `O'chirish`,
         icon: 'pi pi-trash',
         command: () => {
-            console.log(652);
+          modalDelet()
         }
     },
 ];
