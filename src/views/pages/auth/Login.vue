290590c6c49dc6fcd26b99b2aa34e071dbedffa8
UZB02 <muhsinbekmirzamatov3@gmail.com>
2024-04-13 00:11:55 +0500

    Log in sahifasi yangilandi

--- src/views/pages/auth/Login.vue
+++ src/views/pages/auth/Login.vue
@@ -33,6 +33,11 @@ const login = async () => {
         // loading.value = false
     }
 };
+
+const eys=ref(false)
+const showPassword =(()=>{
+    eys.value = !eys.value
+})
 </script>
 
 <template>
@@ -48,7 +53,7 @@ const login = async () => {
                         <h1 class="font-sans text-lg text-red-500 font-medium">{{ error3 }}</h1>
                     </div>
                     <div class="divide-y divide-gray-200">
-                        <form @submit.prevent="login" class="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
+                        <form @submit.prevent="login" class="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7 flex flex-col gap-3">
                             <div class="relative">
                                 <input
                                     autocomplete="off"
@@ -62,29 +67,32 @@ const login = async () => {
                                 <span class="text-red-600 text-sm font-mono">{{ error }}</span>
                                 <label
                                     for="text"
-                                    class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
+                                    class="absolute left-0 -top-3.5 text-gray-600 text-base peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm font-medium"
                                     >Your Email</label
                                 >
                             </div>
                             <div class="relative">
-                                <input
+                                <span class="flex gap-2 items-center justify-center peer placeholder-transparent h-12 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600">
+                                    <input
                                     autocomplete="off"
                                     v-model="password"
                                     id="password"
                                     name="password"
-                                    type="password"
-                                    class="peer placeholder-transparent h-12 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
+                                    :type="eys ? 'text' : 'password'"
+                                    class="w-[90%] outline-none"
                                     placeholder="Password"
                                 />
+                                <i @click="showPassword" :class="eys ? 'pi pi-eye cursor-pointer' : 'pi pi-eye-slash cursor-pointer'" class="text-gray-400"></i>
+                                </span>
                                 <span class="text-red-600 text-sm font-mono">{{ error2 }}</span>
                                 <label
                                     for="password"
-                                    class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
+                                    class="absolute left-0 -top-3.5 text-gray-600  peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm font-medium text-base"
                                     >Password</label
                                 >
                             </div>
                             <div class="relative w-full">
-                                <button type="button" @click="login" class="bg-blue-500 w-full text-white rounded-md px-2 py-3">
+                                <button type="button" @click="login" class="bg-blue-500 text-2xl font-medium w-full text-white rounded-md px-2 py-3">
                                    <span class="flex items-center justify-center">
                                     <span :class="isloading ? 'block' : 'hidden'"><div aria-label="Loading..." role="status" class="flex items-center space-x-2">
     <svg class="h-6 w-6 animate-spin stroke-white" viewBox="0 0 256 256">
