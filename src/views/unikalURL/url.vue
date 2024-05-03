2ebf74af46447389ccab2f6752e83bb83f077fd8
UZB02 <muhsinbekmirzamatov3@gmail.com>
2024-05-03 15:46:09 +0500

    file bo'limi

--- src/views/unikalURL/url.vue
+++ src/views/unikalURL/url.vue
@@ -4,8 +4,8 @@
             <div class="top w-full h-[50vh]">
                 <img class="w-full h-full object-cover rounded-lg" src="https://vp-leads.com/wp-content/uploads/2021/09/professiya-veb-analitik.jpg" alt="">
             </div>
-            <div class="bottom">
-                      <div class="border p-3 rounded-xl">
+            <div class="bottom flex items-center justify-center gap-2">
+                      <div class="border p-3 rounded-xl w-1/2 ">
                         <div class="flex justify-content-between align-items-center mb-2">
                             <h5>Fayllar bo'limi. </h5>
                         </div>
@@ -20,11 +20,11 @@
                         </div>
                             </div>
                         </div>
-                        <ul class="list-none flex flex-col p-0 m-0">
+                        <ScrollPanel style="width: 100%; height: 300px;" >
                             <li  class="flex flex-column border-b-2 p-1 flex-wrap md:flex-row md:align-items-center md:justify-content-between mb-4">
                                 <div class="cards w-full flex items-center justify-between">
                                     <div class="w-[70%] flex items-center gap-3 text-white">
-                                        <Image src="https://primefaces.org/cdn/primevue/images/galleria/galleria10.jpg" alt="Image" width="130" preview />
+                                        <Image src="https://primefaces.org/cdn/primevue/images/galleria/galleria10.jpg" alt="Image" width="100" preview />
                                         <div class="">
                                             <span class="text-900 font-medium mr-2 mb-1 md:mb-0">rasmNomi.jpg</span>
                                             <div class="mt-1 text-600">Rasm haqida ma'lumot</div>
@@ -40,7 +40,55 @@
                             <li  class="flex flex-column border-b-2 p-1 flex-wrap md:flex-row md:align-items-center md:justify-content-between mb-4">
                                 <div class="cards w-full flex items-center justify-between">
                                     <div class="w-[70%] flex items-center gap-3 text-white">
-                                        <Image src="https://primefaces.org/cdn/primevue/images/galleria/galleria10.jpg" alt="Image" width="130" preview />
+                                        <Image src="https://primefaces.org/cdn/primevue/images/galleria/galleria10.jpg" alt="Image" width="100" preview />
+                                        <div class="">
+                                            <span class="text-900 font-medium mr-2 mb-1 md:mb-0">rasmNomi.jpg</span>
+                                            <div class="mt-1 text-600">Rasm haqida ma'lumot</div>
+                                        </div>
+                                    </div>
+                                    <div class="mt-2 pr-5 md:mt-0 flex align-items-center gap-3 cursor-pointer">
+                                    <i class="pi pi-file-edit" v-tooltip.top="'Taxrirlash'"></i>
+                                        <i class="pi pi-trash" v-tooltip.top="`O'chirish`"></i>
+                                        <i class="pi pi-download" v-tooltip.top="'Yuklab olish'"></i>
+                                    </div>
+                                </div>
+                            </li>
+                            <li  class="flex flex-column border-b-2 p-1 flex-wrap md:flex-row md:align-items-center md:justify-content-between mb-4">
+                                <div class="cards w-full flex items-center justify-between">
+                                    <div class="w-[70%] flex items-center gap-3 text-white">
+                                        <Image src="https://primefaces.org/cdn/primevue/images/galleria/galleria10.jpg" alt="Image" width="100" preview />
+                                        <div class="">
+                                            <span class="text-900 font-medium mr-2 mb-1 md:mb-0">rasmNomi.jpg</span>
+                                            <div class="mt-1 text-600">Rasm haqida ma'lumot</div>
+                                        </div>
+                                    </div>
+                                    <div class="mt-2 pr-5 md:mt-0 flex align-items-center gap-3 cursor-pointer">
+                                    <i class="pi pi-file-edit" v-tooltip.top="'Taxrirlash'"></i>
+                                        <i class="pi pi-trash" v-tooltip.top="`O'chirish`"></i>
+                                        <i class="pi pi-download" v-tooltip.top="'Yuklab olish'"></i>
+                                    </div>
+                                </div>
+                            </li>
+                            <li  class="flex flex-column border-b-2 p-1 flex-wrap md:flex-row md:align-items-center md:justify-content-between mb-4">
+                                <div class="cards w-full flex items-center justify-between">
+                                    <div class="w-[70%] flex items-center gap-3 text-white">
+                                        <Image src="https://primefaces.org/cdn/primevue/images/galleria/galleria10.jpg" alt="Image" width="100" preview />
+                                        <div class="">
+                                            <span class="text-900 font-medium mr-2 mb-1 md:mb-0">rasmNomi.jpg</span>
+                                            <div class="mt-1 text-600">Rasm haqida ma'lumot</div>
+                                        </div>
+                                    </div>
+                                    <div class="mt-2 pr-5 md:mt-0 flex align-items-center gap-3 cursor-pointer">
+                                    <i class="pi pi-file-edit" v-tooltip.top="'Taxrirlash'"></i>
+                                        <i class="pi pi-trash" v-tooltip.top="`O'chirish`"></i>
+                                        <i class="pi pi-download" v-tooltip.top="'Yuklab olish'"></i>
+                                    </div>
+                                </div>
+                            </li>
+                            <li  class="flex flex-column border-b-2 p-1 flex-wrap md:flex-row md:align-items-center md:justify-content-between mb-4">
+                                <div class="cards w-full flex items-center justify-between">
+                                    <div class="w-[70%] flex items-center gap-3 text-white">
+                                        <Image src="https://primefaces.org/cdn/primevue/images/galleria/galleria10.jpg" alt="Image" width="100" preview />
                                         <div class="">
                                             <span class="text-900 font-medium mr-2 mb-1 md:mb-0">TZ.doc</span>
                                             <div class="mt-1 text-600">Rasm haqida ma'lumot</div>
@@ -53,17 +101,32 @@
                                     </div>
                                 </div>
                             </li>
-                        </ul>
+                        </ScrollPanel>
+                    </div>
+                    <div class="comants w-1/2">
+
                     </div>
             </div>
         </div>
     </section>
 </template>
-<script>
+<script setup>
 import { ref } from 'vue';
 import router from '../../router';
+import axios from 'axios';
 const url = router.currentRoute.value;
 console.log(url, 88);
+
+function fetchData() {
+    axios.get(`https://vp-leads.com/wp-json/wp/v2/posts/${url.params.id}`, {
+
+        headers: {
+            Authorization: 'Bearer ' + localStorage.getItem('token')
+        }
+    }
+    
+    )
+}
 </script>
 <style></style>
 
