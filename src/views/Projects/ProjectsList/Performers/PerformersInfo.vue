f0750473c9042f5355641ba1de302d9362c9fde9
UZB02 <muhsinbekmirzamatov3@gmail.com>
2024-05-10 15:39:35 +0500

    Performer haqida umumiy ma'lumot tasklar ro'yxati tuzildi

--- src/views/Projects/ProjectsList/Performers/PerformersInfo.vue
+++ src/views/Projects/ProjectsList/Performers/PerformersInfo.vue
@@ -1,6 +1,6 @@
 <template>
     <header class="mb-2 flex items-center ">
-        <img :src="user.avatar" class="w-[50px] h-[50px] rounded-full" alt="">
+        <img :src="user.avatar" class="w-[50px] h-[50px] rounded-full object-cover" alt="">
        <span>
          <h1 class="ml-3 font-bold text-3xl">
             {{ user.fio }}
@@ -9,51 +9,71 @@
        </span>
     </header>
     <section class="flex flex-col gap-2">
-        <div class="left flex gap-2 justify-between">
-            <div class="card p-2 w-1/2 flex flex-col gap-3">
+        <div class="left flex gap-2 max-[780px]:flex-col justify-between">
+            <div class="card p-2 w-1/2 max-[780px]:w-full flex flex-col gap-3">
                 <div class="top flex items-center justify-between gap-2">
-                    <button @click="addProject"
-                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded flex items-center justify-center gap-1"><i
-                    class="pi pi-plus"></i><h3 class="text-white">ADD
-                task</h3></button>
                     <div class="flex items-center shadow rounded border-0 bg-purple-white justify-between">
                         <input type="text" class="p-[10px] outline-none" placeholder="Search...">
                         <i class="pi pi-search mr-2 cursor-pointer"></i>
                     </div>
                     <div class="flex align-items-center justify-content-between">
                         <Dropdown v-model="selectedCity" :options="cities" optionLabel="name"
-                            placeholder="Haftalik topshiriqlar" class="w-1/2 md:w-14rem border" />
+                            placeholder="Haftalik topshiriqlar" class="md:w-14rem border" />
                     </div>
                 </div>
+                <div>
+                    <span class="flex w-full justify-stretch">
+                        <span class="w-1/2 font-bold">
+                            <i class="pi pi-bookmark-fill text-blue-600"></i>
+                            {{ tasks.length }}
+                        </span>
+                        <span class="w-1/2 flex items-center gap-1 font-bold justify-end">
+                            <i class="pi pi-star-fill text-orange-400"></i>
+                            {{ allBall }}
+                        </span>
+                        </span>
+                </div>
                 <Accordion :activeIndex="0">
-                    <AccordionTab v-for="task in tasks" :key="task.title" :header="task.title">
+                    <AccordionTab v-for="item in tasks" :key="item.task.title" :header="item.task.title">
                         <section class="flex flex-col gap-2">
-                            <div class="top flex gap-2">
-                                <div class="left flex items-center w-1/3">
-                                    <Image :src="task.img" alt="Image" preview class="
-                                         w-full object-cover text-white rounded-3xl" />
-                                </div>
-                                <div class="right flex gap-2 w-[75%]">
-                                    <ScrollPanel style="width: 100%; height: 150px">
+                            <div class="top flex flex-col gap-2">
+                                <div class="right flex gap-2 w-[100%]">
+                                    <ScrollPanel style="width: 100%; height: auto; padding: 10px 0;">
                                         <p>
-                                            {{ task.content }}Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                                            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                                            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                                            consequat.
-                                            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                                            fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                                            culpa qui officia deserunt mollit anim id est laborum.
+                                            {{ item.task.description }}
                                         </p>
                                     </ScrollPanel>
                                 </div>
+                                <div class="time flex justify-around gap-2 w-[100%]">
+                                    <div class="flex flex-col gap-2  w-[30%]">
+                                        <label>Topshiriq yaratilgan vaqt</label>
+                                    <span class="text-gray-400 flex items-center justify-around">
+                                        <i class="pi pi-clock"></i>
+                                        {{ item.created_date }}
+                                    </span>
+                                    </div>
+                                    <div class="flex flex-col gap-2  w-[30%]">
+                                        <label>Topshiriq tugatilgan vaqt</label>
+                                    <span class="text-gray-400 flex items-center justify-around">
+                                        <i class="pi pi-clock"></i>
+                                        {{ item.created_date }}
+                                    </span>
+                                    </div>
+                                    <div class="flex flex-col gap-2 items-end w-[30%]">
+                                        <label>Berilgan ball</label>
+                                    <span class="text-gray-400">
+                                        <i class="pi pi-star text-orange-400"></i>
+                                        {{ item.ball }}
+                                    </span>
+                                    </div>
+                                </div>
                             </div>
-                            <ProgressBar :value="task.value"></ProgressBar>
                         </section>
                     </AccordionTab>
                 </Accordion>
                 <Paginator :rows="10" :totalRecords="120" :rowsPerPageOptions="[10, 20, 30]"></Paginator>
             </div>
-            <div class="card h-[48rem] p-2 w-1/2">
+            <div class="card h-[48rem] p-2 w-1/2 max-[780px]:w-full">
                 <span class="flex items-center justify-end">
                     <Dropdown v-model="selectedTime" :options="times" optionLabel="name"
                             placeholder="2024-yil" class=" font-bold md:w-14rem border" />
@@ -80,6 +100,7 @@ const performer_id=ref(router.currentRoute.value.params.performers_id)
 console.log(project_id.value);
 const perFoRmer=ref({})
 const user=ref({})
+const allBall=ref(0) 
 onMounted(() => {
     chartData.value = setChartData();
     chartOptions.value = setChartOptions();
@@ -103,24 +124,7 @@ const times = ref([
     { name: '2024-yil', code: '2024', },
 ]);
 
-const tasks = ref([
-    {
-        title: 'Loyiha ijrochilari ish samarasini kuzatib borish qismi',
-        content: 'Bu yerda topshiriq haqida qisqacha malumot kiritiladi',
-        img: `https://avatars.mds.yandex.net/i?id=2f75325425ed87090ad310da919257e67e4982f3-4961046-images-thumbs&n=13`,
-        value: `33`
-    },
-    {
-        title: 'Loyiha bosqichlari bilan ishlash',
-        content: 'Content 2',
-        img: `https://avatars.mds.yandex.net/i?id=ea4183afac127d5142d083b2a4deee7565b4e93f-3096790-images-thumbs&n=13`,
-        value: `43`
-    },
-    { title: 'Loyiha sprintlari bilan ishlash', content: 'Content 3', img: `https://avatars.mds.yandex.net/i?id=592ac39831c51ec7f2825114cbda4faf5d349e32-10814916-images-thumbs&n=13`, value: `68` },
-    { title: 'Loyiha ijrochilari ish samarasini kuzatib borish qismi', content: 'Bu yerda topshiriq haqida qisqacha malumot kiritiladi', img: `https://avatars.mds.yandex.net/i?id=2f75325425ed87090ad310da919257e67e4982f3-4961046-images-thumbs&n=13`, value: `55` },
-    { title: 'Topshiriq 1', content: 'Content 2', img: `https://avatars.mds.yandex.net/i?id=ea4183afac127d5142d083b2a4deee7565b4e93f-3096790-images-thumbs&n=13`, value: `45` },
-    { title: 'Topshiriq 2', content: 'Content 3', img: `https://avatars.mds.yandex.net/i?id=592ac39831c51ec7f2825114cbda4faf5d349e32-10814916-images-thumbs&n=13`, value: `98` }
-]);
+const tasks = ref({});
 
 function fetchPerFormer() {
     axios
@@ -133,6 +137,8 @@ function fetchPerFormer() {
         }
     }).then((res) => {
         perFoRmer.value=res.data.user_role;
+        tasks.value=res.data.ball;
+        allBall.value=res.data.balls;
         user.value=res.data.user;
         // comunitiCard.value.sort((a,b)=> a.ball + b.ball);
         console.log(res.data);
